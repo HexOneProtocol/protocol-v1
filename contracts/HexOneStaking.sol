c94d478181a1257d41c80a1858cd9f122f61b3fd
Kell Daniel <jonekell1002@gmail.com>
2023-05-11 20:16:18 +0200

    Fixed issue for HOS-02
    Co-authored-by: Cin Drallig <104178930+CinDrallig@users.noreply.github.com>

--- contracts/HexOneStaking.sol
+++ contracts/HexOneStaking.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
@@ -15,7 +14,6 @@ import "./utils/TokenUtils.sol";
 
 contract HexOneStaking is
     OwnableUpgradeable,
-    ERC721HolderUpgradeable,
     ReentrancyGuardUpgradeable,
     IHexOneStaking
 {
